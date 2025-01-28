036b81d07df8efaa1ab6a735fd2c473cd6c3f917
sasek2021 <sasek2021@gmail.com>
2025-01-28 18:26:22 +0700

    update main code

--- main.py
+++ main.py
@@ -163,4 +163,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Run Flask app locally
+    app.run(debug=False, host='0.0.0.0', port=5000)  # Run Flask app locally
